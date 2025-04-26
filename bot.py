877dcdee8760729b38522448000fa1f0ff3f2d59
elmonoloko <35399570+elmonoloko@users.noreply.github.com>
2025-04-25 19:40:53 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,22 +1,21 @@
+import os
 import discord
 from discord.ext import commands
 
-# Configura los "intents" (permisos del bot)
+# Configura los "intents"
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Crea la instancia del bot
+# Crea el bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Evento que se ejecuta cuando el bot se conecta
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user}')
 
-# Comando para responder cuando escriben !terriblemala
 @bot.command()
 async def terriblemala(ctx):
     await ctx.send("¡Este es el comando de prueba de terriblemala!")
 
-# Ejecuta el bot usando el token (pon tu token aquí)
-bot.run('TU_TOKEN_AQUI')
+# Ejecutar el bot con el token desde una variable de entorno
+bot.run(os.getenv('MTM2NTQ5ODQ0MzY5ODUzNjUyOQ.GUfFYR.9VdnzHSbkKqKi7T8oEdA2UKARpJE0apNsbmTj8'))
