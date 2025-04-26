e9304a1e9090ce1e11baf200fe626f5111200fec
elmonoloko <35399570+elmonoloko@users.noreply.github.com>
2025-04-25 20:53:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,12 +13,13 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
+# Obtener el token de las variables de entorno
 token = os.getenv("DISCORD_TOKEN_ARELITTA")
+
+# Verifica si el token está disponible
 if token is None:
     raise RuntimeError("El token no está definido en las variables de entorno.")
 else:
-    print("Token cargado correctamente.")  # Esto te permitirá ver si el token se carga bien
+    print("Token cargado correctamente.")
 
 bot.run(token)
-
-
