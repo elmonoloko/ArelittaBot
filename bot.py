8009ca8194e6b4e2bebbce596b2e95517b8bd04d
elmonoloko <35399570+elmonoloko@users.noreply.github.com>
2025-04-25 20:34:49 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@ import os
 from discord.ext import commands
 from discord import Intents
 
+# Configura los intents
 intents = Intents.default()
-intents.members = True
-intents.message_content = True
+intents.members = True  # Para recibir eventos de miembros
+intents.message_content = True  # Para recibir el contenido de los mensajes
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -12,6 +13,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
+# Verifica que el token esté definido correctamente en las variables de entorno
 token = os.getenv("DISCORD_TOKEN_ARELITTA")
 if token is None:
     raise RuntimeError("El token no está definido en las variables de entorno.")
