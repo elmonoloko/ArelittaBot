49c8f38197ad41327299751238ff3c8671bf21d7
elmonoloko <35399570+elmonoloko@users.noreply.github.com>
2025-04-25 20:12:14 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,11 @@
 import os
-import discord
 from discord.ext import commands
 
-# Configura los "intents"
-intents = discord.Intents.default()
-intents.message_content = True
-
-# Crea el bot
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
-    print(f'Bot conectado como {bot.user}')
+    print(f'Logged in as {bot.user}')
 
-@bot.command()
-async def terriblemala(ctx):
-    await ctx.send("¡Este es el comando de prueba de terriblemala!")
-
-# Ejecutar el bot con el token desde una variable de entorno
-bot.run('MTM2NTQ5ODQ0MzY5ODUzNjUyOQ.GUfFYR.9VdnzHSbkKqKi7T8oEdA2UKARpJE0apNsbmTj8')
+# Asegúrate de que el token provenga de la variable de entorno
+bot.run(os.getenv("DISCORD_TOKEN"))
